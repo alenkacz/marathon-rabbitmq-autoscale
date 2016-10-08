0e3c9abe1e94f85190687cece5180e8236b5db0a
Alena Varkockova <varkockova.a@gmail.com>
2016-10-08 14:12:39 +0200

    Add possibility to specify instances count

--- src/main/scala/cz/alenkacz/marathon/scaler/Main.scala
+++ src/main/scala/cz/alenkacz/marathon/scaler/Main.scala
@@ -5,12 +5,11 @@ import com.rabbitmq.client._
 
 import scala.collection.JavaConversions._
 import com.typesafe.scalalogging.StrictLogging
-import mesosphere.marathon.client.model.v2.App
 import mesosphere.marathon.client.{Marathon, MarathonClient}
 
 object Main extends StrictLogging {
 
-  case class Application(name: String, queueName: String, limit: Int)
+  case class Application(name: String, queueName: String, limit: Int, maxInstancesCount: Option[Int] = None)
 
   private def isOverLimit(rmqConnection: Channel, queueName: String, limit: Int) = rmqConnection.messageCount(queueName) > limit
 
@@ -31,16 +30,16 @@ object Main extends StrictLogging {
     rmqConnection.createChannel()
   }
 
-  def scaleUp(marathonClient: Marathon, applicationName: String): Unit = {
+  def scaleUp(marathonClient: Marathon, applicationName: String, maxInstancesCount: Option[Int]): Unit = {
     val applicationState = marathonClient.getApp(applicationName).getApp
-    applicationState.setInstances(applicationState.getInstances + 1)
+    applicationState.setInstances(Math.min(applicationState.getInstances + 1, maxInstancesCount.getOrElse(Integer.MAX_VALUE)))
     marathonClient.updateApp(applicationName, applicationState, true)
   }
 
   def checkAndScale(applications: Seq[Application], rmqChannel: Channel, marathonClient: Marathon): Unit = {
     applications.foreach(app => {
       isOverLimit(rmqChannel, app.queueName, app.limit) match {
-        case true => scaleUp(marathonClient, app.name)
+        case true => scaleUp(marathonClient, app.name, app.maxInstancesCount)
         case false =>
       }
     })
@@ -51,6 +50,15 @@ object Main extends StrictLogging {
     val rmqChannelConnection = rmqConnect(config.getConfig("rabbitMq"))
     val marathonClient = MarathonClient.getInstance(config.getConfig("marathon").getString("url"))
 
-    checkAndScale(config.getConfigList("applications").map(a => Application(a.getString("name"), a.getString("queue"), a.getInt("limit"))), rmqChannelConnection, marathonClient)
+    checkAndScale(config.getConfigList("applications").map(a => Application(a.getString("name"), a.getString("queue"), a.getInt("limit"), a.getOptionalInt("maxInstancesCount"))), rmqChannelConnection, marathonClient)
+  }
+
+  implicit class RichConfig(val underlying: Config) extends AnyVal {
+    def getOptionalInt(path: String): Option[Int] = if (underlying.hasPath(path)) {
+      Some(underlying.getInt(path))
+    } else {
+      None
+    }
   }
 }
+
--- src/test/scala/cz/alenkacz/marathon/scaler/MainTest.scala
+++ src/test/scala/cz/alenkacz/marathon/scaler/MainTest.scala
@@ -5,7 +5,7 @@ import cz.alenkacz.marathon.scaler.Main.Application
 import mesosphere.marathon.client.Marathon
 import mesosphere.marathon.client.model.v2.GetAppResponse
 import org.junit.runner.RunWith
-import org.mockito.Matchers
+import org.mockito.{ArgumentMatcher, ArgumentMatchers, Matchers, Mockito}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
@@ -16,7 +16,7 @@ class MainTest extends TestFixture with MockitoSugar {
     val marathonMock = mock[Marathon]
     Main.checkAndScale(Array(Application("test", "test", 10)), fixture.rmqChannel, marathonMock)
 
-    verify(marathonMock, never()).updateApp(Matchers.any(), Matchers.any(), Matchers.any())
+    verify(marathonMock, never()).updateApp(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any())
   }
 
   it should "call marathon when limit is reached" in { fixture =>
@@ -25,17 +25,30 @@ class MainTest extends TestFixture with MockitoSugar {
     when(marathonMock.getApp("test")).thenReturn(nonEmptyAppResponse())
     Main.checkAndScale(Array(Application("test", "test", 10)), fixture.rmqChannel, marathonMock)
 
-    verify(marathonMock, atLeastOnce()).updateApp(Matchers.any(), Matchers.any(), Matchers.any())
+    verify(marathonMock, atLeastOnce()).updateApp(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any())
+  }
+
+  it should "not scale to more than maxInstancesCount" in { fixture =>
+    sendMessages(fixture.rmqChannel, "test", 15)
+    val marathonMock = mock[Marathon]
+    when(marathonMock.getApp("test")).thenReturn(nonEmptyAppResponse())
+    Main.checkAndScale(Array(Application("test", "test", 10, Some(1))), fixture.rmqChannel, marathonMock)
+
+    verify(marathonMock, atLeastOnce()).updateApp(ArgumentMatchers.any(), ArgumentMatchers.argThat(new AppWithInstancesCount(1)), ArgumentMatchers.any())
   }
 
   private def sendMessages(rmqChannel: Channel, queueName: String, number: Int): Unit = {
     1 to number foreach {_ => rmqChannel.basicPublish("", queueName, false, false, null, "test".getBytes) }
   }
 
-  private def nonEmptyAppResponse() = {
+  class AppWithInstancesCount(count: Int) extends ArgumentMatcher[mesosphere.marathon.client.model.v2.App] {
+    override def matches(argument: mesosphere.marathon.client.model.v2.App): Boolean = argument.getInstances == count
+  }
+
+  private def nonEmptyAppResponse(instancesCount: Int = 1) = {
     val response = new GetAppResponse
     response.setApp(new mesosphere.marathon.client.model.v2.App)
-    response.getApp.setInstances(1)
+    response.getApp.setInstances(instancesCount)
     response
   }
 }
