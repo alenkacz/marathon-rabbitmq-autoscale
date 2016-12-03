0f6f4d804a233799a890bf03395ec8ee1cbb7608
Alena Varkockova <varkockova.a@gmail.com>
2016-12-03 10:23:58 +0100

    Trust all SSL connections

--- src/main/scala/cz/alenkacz/marathon/scaler/rabbitmq/Client.scala
+++ src/main/scala/cz/alenkacz/marathon/scaler/rabbitmq/Client.scala
@@ -1,11 +1,18 @@
 package cz.alenkacz.marathon.scaler.rabbitmq
 
+import java.net.URL
+import java.security.cert.X509Certificate
+
 import com.rabbitmq.http.client.{Client => HttpClient}
+import org.apache.http.conn.ssl.{SSLConnectionSocketFactory, SSLContextBuilder, SSLSocketFactory, TrustStrategy}
 
 import scala.util.{Failure, Success, Try}
 
 class Client(apiUrl: String, username: String, password: String) {
-  lazy val client = new HttpClient(apiUrl, username, password)
+  lazy val trustAllSslContext =  new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+    override def isTrusted(chain: Array[X509Certificate], authType: String): Boolean = true
+  }).build()
+  lazy val client = new HttpClient(new URL(apiUrl), username, password, trustAllSslContext)
 
   def queueExists(vhost: String, queueName: String): Try[Boolean] = {
     Try(client.getQueue(vhost, queueName) match {
